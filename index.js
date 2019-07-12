ebe592744ea515ce730318d0345771df990767cb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-12 09:35:48 +0200

    Feature: added server.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = express();
+const server = require(".server");
 
 server.listen(4000, () => {
   console.log("server is running on http//:localhost:4000");
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const ActionRouter = require("./data/helpers/actionModel");
+const ProjectRouter = require("./data/helpers/projectModel");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/action", ActionRouter);
+server.use("/api/project", ProjectRouter);
+
+module.exports = server;
